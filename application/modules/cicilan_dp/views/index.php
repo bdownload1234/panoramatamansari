e70339368d01b13f92f1cf4e05b6d615a34a52c1
bdownload1234 <bdownload1234@gmail.com>
2024-11-20 10:46:38 +0700

    [Update] Implement total down payment calculation and remaining obligation adjustment

--- application/modules/cicilan_dp/views/index.php
+++ application/modules/cicilan_dp/views/index.php
@@ -358,6 +358,14 @@
             var sisa_kewajiban = (harga_jual_ajb_kredit-harga_acc_bank);
             sisa_kewajiban = sisa_kewajiban.toString();
             $('#sisa_kewajiban').val(formatRupiah(sisa_kewajiban, ''));
+
+            var total_dp = 0;
+            $('.dp').each(function(){
+                // if nan, set to 0
+                total_dp += parseInt(($(this).val()).replaceAll(',', '')) || 0;
+            });
+
+            var kekurangan = (sisa_kewajiban-total_dp);
         })
         
         $('#dpTable').on('keyup', '.dp', function(){
